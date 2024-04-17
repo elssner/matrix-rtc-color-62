1373b6d0b12559d760affd6c615996d07cbc28d0
GTA Calliope <73078489+elssner@users.noreply.github.com>
2024-04-17 17:54:15 +0200

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,16 +4,26 @@ input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
 input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
     rtcpcf85063tp.writeDateTime(rtcpcf85063tp.rtcpcf85063tp_eADDR(rtcpcf85063tp.eADDR.RTC_x51), rtcpcf85063tp.rtcpcf85063tp_eRegister(rtcpcf85063tp.eRegister.Sekunde), [0, 1 + rtcpcf85063tp.getByte(rtcpcf85063tp.rtcpcf85063tp_eRegister(rtcpcf85063tp.eRegister.Minute), rtcpcf85063tp.eFormat.DEC)])
 })
+let rgbSekunde = 0
 matrix.init(matrix.ePages.y64)
-matrix.writeDisplay()
+matrix.displayMatrix()
 rtcpcf85063tp.beimStart(rtcpcf85063tp.rtcpcf85063tp_eADDR(rtcpcf85063tp.eADDR.RTC_x51))
 loops.everyInterval(1000, function () {
     rtcpcf85063tp.readDateTime(rtcpcf85063tp.rtcpcf85063tp_eADDR(rtcpcf85063tp.eADDR.RTC_x51))
+    rtcpcf85063tp.anzeige25LED(rtcpcf85063tp.e25LED.Zeit)
     if (rtcpcf85063tp.isChanged(rtcpcf85063tp.rtcpcf85063tp_eRegister(rtcpcf85063tp.eRegister.Minute))) {
         matrix.clearMatrix(0, 5)
         matrix.writeClock_radius24(30, 23, rtcpcf85063tp.getByte(rtcpcf85063tp.rtcpcf85063tp_eRegister(rtcpcf85063tp.eRegister.Stunde), rtcpcf85063tp.eFormat.DEC), rtcpcf85063tp.getByte(rtcpcf85063tp.rtcpcf85063tp_eRegister(rtcpcf85063tp.eRegister.Minute), rtcpcf85063tp.eFormat.DEC))
-        matrix.writeDisplay(0, 5, matrix.eI2C.I2C_x3C)
+        matrix.displayMatrix(0, 5, matrix.eI2C.I2C_x3C)
     }
-    matrix.writeDigitImageArray(rtcpcf85063tp.getTime(rtcpcf85063tp.ePart.mit), 1, 49, 11, 0, matrix.eTransparent.u, matrix.oled_eFaktor(matrix.eFaktor.f2))
-    matrix.writeDisplay(6, 7, matrix.eI2C.I2C_x3C)
+    matrix.writeDigits(6, 0, rtcpcf85063tp.getTime(rtcpcf85063tp.ePart.mit), 11, 0, matrix.eTransparent.u, matrix.matrix_eFaktor(matrix.eFaktor.f2))
+    matrix.displayMatrix(6, 7, matrix.eI2C.I2C_x3C)
+    rgbSekunde = Math.trunc(rtcpcf85063tp.getByte(rtcpcf85063tp.rtcpcf85063tp_eRegister(rtcpcf85063tp.eRegister.Sekunde), rtcpcf85063tp.eFormat.DEC) / 5)
+    for (let Index = 0; Index <= 11; Index++) {
+        if (Index == rgbSekunde && rtcpcf85063tp.getByte(rtcpcf85063tp.rtcpcf85063tp_eRegister(rtcpcf85063tp.eRegister.Sekunde), rtcpcf85063tp.eFormat.einer) % 2 == 0) {
+            CalliColor.ShowColorOnPixelbright(CalliColor.rgb(0, 255, 0), Index)
+        } else {
+            CalliColor.ShowColorOnPixelbright(0x000000, Index)
+        }
+    }
 })
